00d13639b8e61733f1f4dab6a7c61a57118b9cd4
bishantkc <bishantkc888@gmail.com>
2024-01-09 15:14:08 +0545

    Checking multiple of 5

---
+++ javascript_basic_exercises/check_number_multiple_of_5_from_prompt_user.js
@@ -0,0 +1,9 @@
+/* answer can be printed by connecting this file with html and prinitng in console of website */
+
+let num = prompt("Enter a Number")
+
+if (num % 5 == 0) {
+    console.log(num, "is a multiple of 5")
+} else {
+    console.log(num, "is not a multiple of 5")
+}
